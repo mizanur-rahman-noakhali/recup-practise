9964e29f87a5023109df40bdd6dafd89a1313e4e
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-05-31 22:57:22 +0600

    practise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [nayoks,setNayoks]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=>setNayoks(data));
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {
+        nayoks.map(nayok=> <Nayok id={nayok.id} key={nayok.id} title={nayok.title} body={nayok.body}></Nayok>)
+         }
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
+}
+
+function Nayok(props){
+  const comStyle={
+    border:'5px solid blue',
+    margin:'5px'
+  }
+  return(
+    <div style={comStyle}>
+      <h1>Company id:{props.id}</h1>
+      <h1> Employ title:{props.title}</h1>
+      <h1> some body:{ props.body}</h1>
+
+    </div>
+  )
 }
 
 export default App;
